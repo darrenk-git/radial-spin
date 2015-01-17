1f9876db1df20f5e3b76f6d0bac75a7207138173
darrenk-git <darrenk-git@users.noreply.github.com>
2015-01-17 12:06:10 +0000

    tidy up
    Tidied up the code a bit, added some more comments.
    Included a counter that resets every cycle of the rotation to prevent
    number gremlins from invading peoples computers

--- radial-spin.js
+++ radial-spin.js
@@ -1,4 +1,12 @@
-  
+ /*
+
+Author: Darren Kearney
+
+Description: This is a modified version of the radial menu found here http://creative-punch.net/2014/02/making-animated-radial-menu-css3-javascript/   Thank you good peoples!
+
+*/
+
+ 
 document.querySelector('.menu-button').onclick = function(e) {
  
   e.preventDefault();
@@ -7,144 +15,32 @@ document.querySelector('.menu-button').onclick = function(e) {
   //document.querySelector('.circle').classList.toggle('bounceIn');
 }
 
-//how do you make a global variable?
+//uses a global variable to set the start coordinates for drawing the circle in which to assign the link elements
 
-var c = 0;
+var c = -0.45; //sets the start point to be a quarter circle from sine wave start (the top)
  
 function rotate() {
   nIntervId = setInterval(draw, 1000);
 }
 
 function draw() {
-	
-	c = (c - 0.05).toFixed(4); //increments c
-	
-	var items = document.querySelectorAll('.circle a'); //gets the list of links
-	for (var i = 0, l = items.length; i < l; i++) {
   
-	  items[i].style.left = (50 - 60*Math.cos( c * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+  if (c<-2.48) 
+    c = -0.50;
+  else
+    c = (c - 0.05).toFixed(4); //increments c
+    
+	var items = document.querySelectorAll('.circle a'); //gets the list of links
+  
+	for (var i = 0, l = items.length; i < l; i++) {  // sets up an iterator for each item on the list of links and the length of the list
+  
+	  items[i].style.left = (50 - 60*Math.cos( c * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";   // offset - amplitude * cos ( startposition * PI - 2 * (number of link / total links ) * PI ) % ( y coordinate as percentage )
 	  items[i].style.top = (50 + 60*Math.sin( c * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
 
-      }
+  }
 }
  
 function stopRotate() {
   clearInterval(nIntervId);
-  c = 0;
+  c = -0.45;
 }
-
-
-/*
-function tickTock() {
-		nIntervalId = setInterval(shiftXY, 500)	
-}
-
-function shiftXY() {
-	x = 0;
-	do {
-		
-    x = x - 0.05; //increment x
-	console.log('The amount which the circle will be drawn away from it\'s origin point is:  str(x)');
-
-	// draw the list of anchor points
-	for (var i = 0, l = items.length; i < l; i++) {
-  
-	  items[i].style.left = (50 - 60*Math.cos( x * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-	  items[i].style.top = (50 + 60*Math.sin( x * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-
-      }
-	
-	
-	} while (x > -1)
-
-}
-
-function counter(x) {
-	x = x + 0.05;
-	return x;
-}
-
-
-*/
-
-/*
-/////////////////////////
-function rotateCircle() {
- nIntervalId = setInterval(shiftXY, 500)	
-}
-
-function shiftXY() {
-	
-
-		
-	  for (var i = 0, l = items.length; i < l; i++) {
-  
-	  items[i].style.left = (50 - 60*Math.cos( -1*(rotateAmount) * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-	  items[i].style.top = (50 + 60*Math.sin( -1*(rotateAmount) * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-
-      }
-    }
-}
-/////////////////////////
-
-/*
-function rotate(rotateNotch) {
-
-  do {
-
-    for(var i = 0, l = items.length; i < l; i++) {
-  
-	  items[i].style.left = (50 - 60*Math.cos(rotateNotch * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-	  items[i].style.top = (50 + 60*Math.sin(rotateNotch * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-
-    }
-	
-  rotateNotch = rotateNotch - 0.05;
-  
-  } while (rotateNotch > -1)
-
-
-}
-
-rotate(0);*/
-
-
-
-
-/////////////////////////// rotate using a simple sine wave calculation, incrememnting one part of it.
-
-
-
-
-
-
- /*
- //set initial state?
-
-  function frame() { //every interval do this
-  
-  x = 0;
-    do {
-     var items = document.querySelectorAll('.circle a');
-	
-     // write a sin wave calculation to get each elements' top left css attribute that determines it's placement
-     // increment this further along the sin wave by 1 pixel
-    
-	
-     for (var i = 0, l = items.length; i < l; i++) { //(x = 0; x > -1; x=x-0.05)
-		
-	   items[i].style.left = (50 - 35*Math.cos(-(x) * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-	   items[i].style.top = (50 + 35*Math.sin(-(x) * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
-	 }
-    
-	 x = x-0.05;
-  
-    } while (x > -1)
-  }
-
-  var id = setInterval(frame, 10); // draw a new frame every 10 ms
-
- }
-
-rotate(items); //calls the rotate
-*/
